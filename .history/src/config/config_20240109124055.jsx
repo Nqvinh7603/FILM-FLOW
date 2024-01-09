97e32237411a5a50ad13c78c3ebb2649338941c5
vinh <nqvinh7603@gmail.com>
2024-01-09 12:49:13 +0700

    update code

--- .history/src/config/config_20240109124055.jsx
+++ .history/src/config/config_20240109124055.jsx
@@ -1,6 +1,7 @@
 export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export const apiKey = "dae28cb2a8dbebf72e0eacb8a51b947a";
 const tmdpEndpoint = "https://api.themoviedb.org/3/movie";
+const apiKey = "dae28cb2a8dbebf72e0eacb8a51b947a";
 export const tmdbAPI = {
     getMovieList: (type) => `${tmdpEndpoint}/${type}?api_key=${apiKey}`,
 };
